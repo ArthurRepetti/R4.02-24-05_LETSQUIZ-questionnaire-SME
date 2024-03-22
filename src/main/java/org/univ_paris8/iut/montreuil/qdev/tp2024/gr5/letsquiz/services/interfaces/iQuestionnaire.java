54292ff38c7caf2aab6d744feb59345bcbb92c06
robainggg <robincastel2003@gmail.com>
2024-03-22 12:18:50 +0100

    Changements signatures iQuestionnaire

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr5/letsquiz/services/interfaces/iQuestionnaire.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr5/letsquiz/services/interfaces/iQuestionnaire.java
@@ -1,18 +1,19 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.letsquiz.services.interfaces;
 
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.letsquiz.entities.dto.QuestionDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.letsquiz.entities.dto.QuestionnaireDTO;
 
 import java.util.Set;
 
 public interface iQuestionnaire {
 
-    public void ajouterQuestion(QuestionDTO question);
+    public void ajouterQuestion(QuestionDTO question, QuestionnaireDTO questionnaire);
 
-    public void supprimerQuestion(QuestionDTO question);
+    public void supprimerQuestion(QuestionDTO question, QuestionnaireDTO questionnaire);
 
-    public QuestionDTO trouverQuestionParId(Integer id);
+    public QuestionDTO trouverQuestionParId(Integer id, QuestionnaireDTO questionnaire);
 
-    public Integer obtenirNombreQuestions();
+    public Integer obtenirNombreQuestions(QuestionnaireDTO questionnaire);
 
 
 }
--- src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr5/letsquiz/services/impls/iQuestionnaireTest.java
+++ src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr5/letsquiz/services/impls/iQuestionnaireTest.java
@@ -1,10 +1,19 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.letsquiz.services.impls;
 
 import junit.framework.TestCase;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.letsquiz.entities.dto.QuestionDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.letsquiz.entities.dto.QuestionnaireDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.letsquiz.utils.Langue;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.letsquiz.utils.Niveau;
 
 public class iQuestionnaireTest extends TestCase {
 
+
     public void testAjouterQuestion() {
+        QuestionDTO question = new QuestionDTO(1, "fr", "De quel petit objet se munit le golfeur pour surélever sa balle avant de la frapper?", "Tee", "1","Bah parce que", "https://fr.wikipedia.org/wiki/Matériel_de_golf");
+        QuestionnaireDTO questionnaire = new QuestionnaireDTO();
+
+
     }
 
     public void testSupprimerQuestion() {
