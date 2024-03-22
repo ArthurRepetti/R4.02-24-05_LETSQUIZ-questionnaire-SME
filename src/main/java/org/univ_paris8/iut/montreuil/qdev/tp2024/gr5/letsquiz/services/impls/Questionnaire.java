1722c24d426b668bfe37ee895111a0c7011acaeb
ArthuroofGangstat <arthur.repetti@gmail.com>
2024-03-22 12:10:33 +0100

    création classe questionnaire

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr5/letsquiz/services/impls/Questionnaire.java
@@ -0,0 +1,25 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.letsquiz.services.impls;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.letsquiz.entities.dto.QuestionDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.letsquiz.services.interfaces.iQuestionnaire;
+
+public class Questionnaire implements iQuestionnaire {
+    @Override
+    public void ajouterQuestion(QuestionDTO question) {
+
+    }
+
+    @Override
+    public void supprimerQuestion(QuestionDTO question) {
+
+    }
+
+    @Override
+    public QuestionDTO trouverQuestionParId(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Integer obtenirNombreQuestions() {
+        return null;
+    }
+}
