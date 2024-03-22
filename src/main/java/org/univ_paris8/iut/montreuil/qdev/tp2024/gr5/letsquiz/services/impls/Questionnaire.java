0f11e303b90452090a76e3f492ee82024f229e52
ArthuroofGangstat <arthur.repetti@gmail.com>
2024-03-22 12:21:47 +0100

    modif impl questionnaire

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr5/letsquiz/services/impls/Questionnaire.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr5/letsquiz/services/impls/Questionnaire.java
@@ -1,25 +1,27 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.letsquiz.services.impls;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.letsquiz.entities.dto.QuestionDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.letsquiz.entities.dto.QuestionnaireDTO;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.letsquiz.services.interfaces.iQuestionnaire;
 
 public class Questionnaire implements iQuestionnaire {
+
     @Override
-    public void ajouterQuestion(QuestionDTO question) {
+    public void ajouterQuestion(QuestionDTO question, QuestionnaireDTO questionnaire) {
 
     }
 
     @Override
-    public void supprimerQuestion(QuestionDTO question) {
+    public void supprimerQuestion(QuestionDTO question, QuestionnaireDTO questionnaire) {
 
     }
 
     @Override
-    public QuestionDTO trouverQuestionParId(Integer id) {
+    public QuestionDTO trouverQuestionParId(Integer id, QuestionnaireDTO questionnaire) {
         return null;
     }
 
     @Override
-    public Integer obtenirNombreQuestions() {
+    public Integer obtenirNombreQuestions(QuestionnaireDTO questionnaire) {
         return null;
     }
 }
