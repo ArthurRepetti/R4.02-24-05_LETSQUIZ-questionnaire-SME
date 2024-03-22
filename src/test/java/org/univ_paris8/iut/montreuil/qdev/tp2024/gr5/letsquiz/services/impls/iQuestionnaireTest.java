052db00fdef4df1fd42ff122f8fcd928dee947b1
ArthuroofGangstat <arthur.repetti@gmail.com>
2024-03-22 12:03:33 +0100

    déplacement iQuestionnaire et tests

---
+++ src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr5/letsquiz/services/impls/iQuestionnaireTest.java
@@ -0,0 +1,18 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr5.letsquiz.services.impls;
+
+import junit.framework.TestCase;
+
+public class iQuestionnaireTest extends TestCase {
+
+    public void testAjouterQuestion() {
+    }
+
+    public void testSupprimerQuestion() {
+    }
+
+    public void testTrouverQuestionParId() {
+    }
+
+    public void testObtenirNombreQuestions() {
+    }
+}
